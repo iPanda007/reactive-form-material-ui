942b0938ed9b898dd7acca02ac5f59e07da732ab
iPanda <aungkyawkhaing2000@gmail.com>
2022-12-03 17:23:49 +0630

    u

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {useForm ,Controller} from 'react-hook-form';
-import {yupResolver} from '@hookform/resolvers/yup';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { filledInputClasses, TextField } from '@mui/material'
 import * as yup from 'yup';
 
 const schema = yup.object().shape({
   email: yup.string().email().required(),
-  password:yup.string().min(4).max(20).required(),
+  password: yup.string().min(4).max(20).required(),
 })
 
 let App = () => {
@@ -15,32 +16,44 @@ let App = () => {
     handleSubmit,
     watch,
     control,
-    formState:{errors}
-  }  = useForm({
-    resolver:yupResolver(schema)
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schema)
   });
 
-  const formSubmitHandler = (data)=>{
-    console.log("form data is",data)
+  const formSubmitHandler = (data) => {
+    console.log("form data is", data)
   }
   console.log(errors)
 
   return (
     <div>
-         <form onSubmit={handleSubmit(formSubmitHandler)}>
-              <Controller
-                 name
-              />
-             <input type="email"  {...register('email')} id="" defaultValue={"aungkyawkhaing2000@gmail.com"} />
-             <br />
-             <input className={`${errors.password && 'red'}`} {...register('password',{required:true})} defaultValue={""} type="password"  />
-             <br />
-              {errors.password && <span>{errors.password.message}</span>}
-              <br />
-             <button type='submit'>Submit</button>
-         </form>
+      <form onSubmit={handleSubmit(formSubmitHandler)}>
+        <Controller
+          name='email'  defaultValue="aungkyawkhaing2000@gmail.com"  control={control}
+          render={({ field }) => (
+            <TextField 
+            {...field} 
+            type="email" 
+            id="" 
+            label="Email" 
+            variant='outlined'
+            error ={!!errors.email}
+            helperText={errors.email ? errors.email?.message : ''}
+            />
+
+          )}
+        />
+
+        <br />
+        <input className={`${errors.password && 'red'}`} {...register('password', { required: true })} defaultValue={""} type="password" />
+        <br />
+        {errors.password && <span>{errors.password.message}</span>}
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
-} 
+}
 
 export default App
